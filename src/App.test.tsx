a65b7a843e7864f0c1ead27ddf1dc32bbd408265
nickkossolapov <nickkossolapov@gmail.com>
2019-02-26 17:28:56 +0200

    Update gitignore and project config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import AppContainer from './app';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import AppContainer from './app';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<AppContainer />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AppContainer />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
